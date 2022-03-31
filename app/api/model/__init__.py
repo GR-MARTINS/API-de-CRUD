827f11744020ed89afede9933ec2b8321d45e2b8
Glayton Martins <gr.martins@outlook.com>
2022-03-31 13:42:59 -0300

    create users table on models

--- app/api/model/__init__.py
+++ app/api/model/__init__.py
@@ -1,4 +1,5 @@
 from app.api.db import db
+from passlib.hash import pbkdf2_sha512
 
 
 class Book(db.Model):
@@ -6,3 +7,16 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     livro = db.Column(db.Unicode)
     escritor = db.Column(db.Unicode)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    escritor = db.Column(db.Unicode, nullable=False)
+    password = db.Column(db.Unicode, nullable=False)
+
+    def gen_hash(self):
+        self.password = pbkdf2_sha512(self.password)
+
+    def verify_password(self, password):
+        return pbkdf2_sha512.verify(password, self.password)

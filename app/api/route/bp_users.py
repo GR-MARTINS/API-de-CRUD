318e4734e58a961761698cfdae57e12094798fa7
Glayton Martins <gr.martins@outlook.com>
2022-04-07 21:32:01 -0300

    creating user controllers

--- app/api/route/bp_users.py
+++ app/api/route/bp_users.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request, current_app, jsonify
 from app.api.schema.schemas import UserSchema
 from app.api.doc import spec, model
 from flask_pydantic_spec import Response, Request
+from app.api.model import User
 
 
 bp = Blueprint('users', __name__)
@@ -16,7 +17,7 @@ def register():
     us = UserSchema()
 
     user = us.load(request.json)
-    #import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
     user.gen_hash()
 
     current_app.db.session.add(user)
@@ -27,14 +28,28 @@ def register():
 
 @bp.get('/api/v1/usuarios')
 def read_users():
-    ...
+    us = UserSchema(many=True)
+    result = User.query.all()
+    return us.jsonify(result), 200
 
 
-@bp.put('/api/v1/usuarios')
-def update():
-    ...
+@bp.put('/api/v1/usuarios/<identificator>')
+def update(identificator):
+    us = UserSchema()
+
+    query = User.query.filter(User.id == identificator)
+    try:
+        lista = query[0]
+    except IndexError:
+        return {"message": "response validation error"}, 404
+
+    query.update(request.json)
+    current_app.db.session.commit()
+    return us.jsonify(query.first()), 201
 
 
-@bp.delete('/api/v1/usuarios')
-def delete():
-    ...
+@bp.delete('/api/v1/usuarios/<identificator>')
+def delete(identificator):
+    User.query.filter(User.id == identificator).delete()
+    current_app.db.session.commit()
+    return jsonify('deletado')
